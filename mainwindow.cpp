f843a0bcf84244aa53adc37833192e8083248892
ichputzhiernur <ichputzhiernur@nettesache.de>
2025-02-10 18:52:23 +0100

    Adds option "invert-gerbers"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -92,6 +92,7 @@ MainWindow::MainWindow(QWidget *parent) :
     args[ MILLARGS ].insert("offset", ui->offsetDoubleSpinBox);
     args[ MILLARGS ].insert("voronoi", ui->voronoiCheckBox);
     args[ MILLARGS ].insert("extra-passes", ui->extrapassesSpinBox);
+    args[ MILLARGS ].insert("invert-gerbers", ui->invertCheckBox);
 
     args[ DRILLARGS ].insert("zdrill", ui->zdrillDoubleSpinBox);
     args[ DRILLARGS ].insert("drill-feed", ui->drillfeedSpinBox);
